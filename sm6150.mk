be5e554b6a003089ed64b75a7721bcf7cab9984b
Cyber Knight <cyberknight755@gmail.com>
2023-02-26 05:12:04 +0800

    mojito: Build DisplayFeatures
    - Add DisplayFeatures to PRODUCT_PACKAGES.
    - Address DisplayFeatures related SEPolicy rules.
    - Define sysfs_displayfeatures as a domain.
    - Allow system_app to read/write vendor_sysfs_graphics
    - Allow system_app to search vendor_sysfs_graphics
    - Setup permission of nodes relevant to DisplayFeatures at boot.
    - Setup DC Dim and HBM nodes as RRO for DisplayFeatures.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    [fiqri19102002: Import source code for build DisplayFeatures app]
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -113,13 +113,18 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    Aperture
+    Aperture \
+    MojitoDisplayFeatures
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
+# DisplayFeatures
+PRODUCT_PACKAGES += \
+    DisplayFeatures
 
 # Charger
 PRODUCT_PACKAGES += \
