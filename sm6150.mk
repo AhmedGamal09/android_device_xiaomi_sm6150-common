0cf4b90974d94b2d170d5647c803068939d806cd
unsatifsed27 <unsatifsederror@gmail.com>
2023-07-06 19:58:01 +0200

    sm6150-common: added viper4android.fx
    Viper's Audio is software for various platforms, such as Android and PC, Cat. It is used to improve the sound in
    general, with the help of various sound effects, i.e. it is actually a system equalizer.
    
    Reference:
    https://github.com/begonia-crdroid/android_packages_apps_ViPER4AndroidFX
    
    sm8250-common: sepolicy: Address denials for ViPER4Android FX
    
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>
    Signed-off-by: Ankit Savani <ankitmsavani12345@gmail.com>
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -80,6 +80,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# ViPER4Android FX
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
